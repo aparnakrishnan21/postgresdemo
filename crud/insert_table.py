5332b0376b6f0af7b473616467385417100eebc0
aparnakrishnan21 <aparnakrishnan21@gmail.com>
2026-06-08 05:30:36 +0530

    insert_table.py for inserting value to the table

---
+++ crud/insert_table.py
@@ -0,0 +1,30 @@
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+from db import get_db_connection
+
+
+def create_user(user_id, username):
+    conn = get_db_connection()
+
+    if conn:
+        try:
+            cursor = conn.cursor()
+
+            query = "INSERT INTO users (id, name) VALUES (%s, %s);"
+            cursor.execute(query, (user_id, username))
+
+            conn.commit()
+            cursor.close()
+
+            print(f"User {username} created successfully.")
+
+        except Exception as e:
+            print(f"Error creating user: {e}")
+
+        finally:
+            conn.close()
+
+
+create_user(1, "Aparna")
